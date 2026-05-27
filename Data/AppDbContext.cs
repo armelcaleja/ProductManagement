055658830d420fada75cfc9bf2d64782b9b9c410
Armel Caleja <armel.d.caleja@gmail.com>
2026-05-27 19:15:22 +0800

    Created AppDbContext class

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=DESKTOP-QEG9QV3;Database=ProductManagementDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
